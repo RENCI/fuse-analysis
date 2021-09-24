c4d973615cc9253479e318cdeb6f30c434e0185f
Jason Reilly <jdr0887@gmail.com>
2021-09-24 15:00:23 -0400

    adding mongodb, adding email to run_with_uploaded_data, adding get_task_ids, adding get_run_parameters

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import aiofiles
 import uuid
 import docker
 import inspect
+import pymongo
+import json
 from typing import Optional, Type
 from fastapi import FastAPI, File, UploadFile, Form, Depends, HTTPException, responses
 from fastapi.params import Param
@@ -14,6 +16,7 @@ from multiprocessing import Process
 from redis import Redis
 from rq import Connection, Queue, Worker
 from rq.job import Job
+
 
 def as_form(cls: Type[BaseModel]):
     new_params = [
@@ -34,6 +37,7 @@ def as_form(cls: Type[BaseModel]):
     setattr(cls, "as_form", _as_form)
     return cls
 
+
 @as_form
 class Parameters(BaseModel):
     SampleNumber: int = 32
@@ -48,6 +52,10 @@ class Parameters(BaseModel):
     ValueLow: int = 5
     ValueHigh: int = 5
 
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+
 app = FastAPI()
 
 app.add_middleware(
@@ -58,32 +66,68 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-client = docker.from_env() 
+client = docker.from_env()
 
 # queue
 redis_connection = Redis(host='redis', port=6379, db=0)
 q = Queue(connection=redis_connection, is_async=True)
 
+mongo_client = pymongo.MongoClient('mongodb://%s:%s@tx-persistence:27017/test' % (os.getenv('MONGO_NON_ROOT_USERNAME'), os.getenv('MONGO_NON_ROOT_PASSWORD')))
+mongo_db = mongo_client["test"]
+mongo_db_email_task_mapping_column = mongo_db["email_task_mapping"]
+
+
 def initWorker():
     worker = Worker(q, connection=redis_connection)
     worker.work()
 
+
 @app.post("/cellfie/run/upload_data")
-async def run_with_uploaded_data(parameters: Parameters = Depends(Parameters.as_form), data: UploadFile = File(...)):
-    #write data to memory
+async def run_with_uploaded_data(email: str, parameters: Parameters = Depends(Parameters.as_form),
+                                 data: UploadFile = File(...)):
+    # write data to memory
     local_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
     task_id = str(uuid.uuid4())
+
+    email_task_mapping_entry = {"email": email, "task_id": task_id}
+    mongo_db_email_task_mapping_column.insert_one(email_task_mapping_entry)
+
     local_path = os.path.join(local_path, f"{task_id}-data")
     os.mkdir(local_path)
+
+    param_path = os.path.join(local_path, "parameters.json")
+    param_data = parameters.toJSON().replace('\n', '')
+    with open(param_path, 'w', encoding='utf-8') as f:
+        json.dump(param_data, f, ensure_ascii=False, indent=4)
+
     file_path = os.path.join(local_path, "input.csv")
     async with aiofiles.open(file_path, 'wb') as out_file:
         content = await data.read()
         await out_file.write(content)
-    #instantiate task
+
+    # instantiate task
     q.enqueue(run_cellfie_image, task_id=task_id, parameters=parameters, job_id=task_id, job_timeout=600)
-    pWorker = Process(target = initWorker)
+    pWorker = Process(target=initWorker)
     pWorker.start()
     return {"task_id": task_id}
+
+@app.get("/cellfie/get_run_parameters/{task_id}")
+async def get_run_parameters(task_id: str):
+
+    local_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+    local_path = os.path.join(local_path, f"{task_id}-data")
+
+    param_path = os.path.join(local_path, "parameters.json")
+    with open(param_path) as f:
+        loaded_json = json.load(f)
+
+    return loaded_json
+
+@app.get("/cellfie/get_task_ids/{email}")
+async def get_task_ids(email: str):
+    query = {"email": email}
+    ret = list(map(lambda a: a, mongo_db_email_task_mapping_column.find(query, {"_id": 0, "task_id": 1})))
+    return ret
 
 @app.get("/cellfie/status/{task_id}")
 def get_task_status(task_id: str):
@@ -93,6 +137,7 @@ def get_task_status(task_id: str):
     except:
         raise HTTPException(status_code=404, detail="Not found")
 
+
 @app.get("/cellfie/results/{task_id}/{filename}")
 def get_task_result(task_id: str, filename: str):
     local_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
@@ -100,15 +145,18 @@ def get_task_result(task_id: str, filename: str):
     file_path = os.path.join(dir_path, f"{filename}.csv")
     if not os.path.isdir(dir_path) or len(os.listdir(dir_path)) < 5:
         raise HTTPException(status_code=404, detail="Not found")
+
     def iterfile():
         try:
             with open(file_path, mode="rb") as file_data:
                 yield from file_data
         except:
             raise Exception()
+
     response = StreamingResponse(iterfile(), media_type="text/csv")
     response.headers["Content-Disposition"] = "attachment; filename=export.csv"
     return response
+
 
 def run_cellfie_image(task_id: str, parameters: Parameters):
     local_path = os.getenv('HOST_ABSOLUTE_PATH')
@@ -117,13 +165,13 @@ def run_cellfie_image(task_id: str, parameters: Parameters):
     local_values = f"{parameters.PercentileLow} {parameters.PercentileHigh}" if parameters.PercentileOrValue == "percentile" else f"{parameters.ValueLow} {parameters.ValueHigh}"
 
     client.containers.run("hmasson/cellfie-standalone-app:v2",
-        volumes={
-            os.path.join(local_path, f"data/{task_id}-data"): {'bind': '/data', 'mode': 'rw'},
-            os.path.join(local_path, "CellFie/input"): {'bind': '/input', 'mode': 'rw'},
-        },
-        name=task_id,
-        working_dir="/input",
-        privileged=True,
-        remove=True,
-        command=f"/data/input.csv {parameters.SampleNumber} {parameters.Ref} {parameters.ThreshType} {parameters.PercentileOrValue} {global_value} {parameters.LocalThresholdType} {local_values} /data"
-    )
+                          volumes={
+                              os.path.join(local_path, f"data/{task_id}-data"): {'bind': '/data', 'mode': 'rw'},
+                              os.path.join(local_path, "CellFie/input"): {'bind': '/input', 'mode': 'rw'},
+                          },
+                          name=task_id,
+                          working_dir="/input",
+                          privileged=True,
+                          remove=True,
+                          command=f"/data/input.csv {parameters.SampleNumber} {parameters.Ref} {parameters.ThreshType} {parameters.PercentileOrValue} {global_value} {parameters.LocalThresholdType} {local_values} /data"
+                          )
